6dda4bd4b2ce6377d960ef6dd81167d125f6efd9
Selim Dincer <wowselim@live.de>
2023-06-09 19:29:19 +0300

    disable snake case temporarily

--- src/main/java/co/selim/nemrut/MainVerticle.kt
+++ src/main/java/co/selim/nemrut/MainVerticle.kt
@@ -7,7 +7,6 @@ import co.selim.nemrut.web.company.companyModule
 import co.selim.nemrut.web.role.roleModule
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.kotlinModule
@@ -78,7 +77,7 @@ class MainVerticle : CoroutineVerticle(), KoinComponent {
 
   private fun ObjectMapper.configure(vararg modules: JacksonDatabindModule) {
     registerModules(*modules)
-    propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
+    //propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
     disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
   }
