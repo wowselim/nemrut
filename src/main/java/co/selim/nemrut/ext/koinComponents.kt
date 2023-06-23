91414f10b4e9a488cddf89eeb74ce93f261f1351
Selim Dincer <wowselim@live.de>
2023-06-23 18:09:44 +0300

    simplify injectAll

--- src/main/java/co/selim/nemrut/ext/koinComponents.kt
+++ src/main/java/co/selim/nemrut/ext/koinComponents.kt
@@ -1,9 +1,6 @@
 package co.selim.nemrut.ext
 
 import org.koin.core.component.KoinComponent
-import org.koin.mp.KoinPlatformTools
 
-inline fun <reified T : Any> KoinComponent.injectAll(
-  mode: LazyThreadSafetyMode = KoinPlatformTools.defaultLazyMode(),
-): Lazy<List<T>> =
-  lazy(mode) { getKoin().getAll<T>() }
+inline fun <reified T : Any> KoinComponent.injectAll(): Lazy<List<T>> =
+  lazy { getKoin().getAll<T>() }
